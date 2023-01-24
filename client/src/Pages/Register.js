f05549146052d592497bd83d8c0aa49b09c87728
Kedar <kedarmakode1598@gmail.com>
2023-01-24 22:37:33 +0530

    add simple setup for register page

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className=' h-screen text-white'>
+        <div className=' flex flex-col justify-center items-center h-full'>
+            {/* FORM */}
+            <div className=' p-10  bg-blue-300'>
+                <form>
+                    <div>
+                        <label className='block'>First Name</label>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <label className='block'>Last Name</label>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <label className='block'>Email</label>
+                        <input type="email" />
+                    </div>
+                    <div>
+                        <label className='block'>Password</label>
+                        <input type="password" />
+                    </div>
+                    <div>
+                        <label className='block'>About Me</label>
+                        <textarea className='w-full' />
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
