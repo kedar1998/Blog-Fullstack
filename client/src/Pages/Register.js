d01f997338d427c39eaecf5cf6e5dd3baebe6252
Kedar <kedarmakode1598@gmail.com>
2023-01-25 21:47:47 +0530

    add basic design to register page

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,36 +1,37 @@
 import React from 'react'
+import registerImg from '../assets/register.jpg'
 
 const Register = () => {
   return (
-    <div className=' h-screen text-white'>
-        <div className=' flex flex-col justify-center items-center h-full'>
-            {/* FORM */}
-            <div className=' p-10  bg-blue-300'>
-                <form>
-                    <div>
-                        <label className='block'>First Name</label>
-                        <input type="text" />
+    <section className='px-4'>
+        <div className='flex flex-col justify-between items-center mt-20'>
+
+            <div className=' w-full md:w-1/2 bg-blue-300 py-5 rounded-2xl p-10'>
+                <h1 className='text-center text-5xl mb-8 text-white'>Register</h1>
+                <form className='space-y-5'>
+                    <div className='max-w-sm mx-auto'>
+                        <input type="text" placeholder='First Name' className='w-full p-1 rounded-md'/>
                     </div>
-                    <div>
-                        <label className='block'>Last Name</label>
-                        <input type="text" />
+                    <div className='max-w-sm mx-auto'>
+                        <input type="text" placeholder='Last Name' className='w-full p-1 rounded-md'/>
                     </div>
-                    <div>
-                        <label className='block'>Email</label>
-                        <input type="email" />
+                    <div className='max-w-sm mx-auto'>
+                        <input type="email" placeholder='Email' className='w-full p-1 rounded-md'/>
                     </div>
-                    <div>
-                        <label className='block'>Password</label>
-                        <input type="password" />
+                    <div className='max-w-sm mx-auto'>
+                        <input type="password" placeholder='Password' className='w-full p-1 rounded-md'/>
                     </div>
-                    <div>
-                        <label className='block'>About Me</label>
-                        <textarea className='w-full' />
+                    <div className='max-w-sm mx-auto'>
+                        <textarea type="password" placeholder='About Me!!!' className='w-full p-1 rounded-md' rows="5"></textarea>
+                    </div>
+                    <div className='max-w-sm mx-auto'>
+                        <button className='text-center w-full'>Submit</button>
                     </div>
                 </form>
             </div>
+
         </div>
-    </div>
+    </section>
   )
 }
 
