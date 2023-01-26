715c342c7ffd13d5fb742fdca91222f6265d039e
Kedar <kedarmakode1598@gmail.com>
2023-01-26 22:09:43 +0530

    add form state in register page

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,7 +1,31 @@
-import React from 'react'
-import registerImg from '../assets/register.jpg'
+import React, {useState, useEffect} from 'react'
+
 
 const Register = () => {
+    
+    const initialState = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        about: '',
+    }
+
+    const [values, setValues] = useState(initialState)
+
+
+    const handleChange = (e) =>{
+        setValues({...values, [e.target.name]: e.target.value})
+    }
+
+
+    // useEffect(() =>{
+    //     console.log(values);
+    // }, [values])
+
+
+
+
   return (
     <section className='px-4'>
         <div className='flex flex-col justify-between items-center mt-20'>
@@ -10,19 +34,19 @@ const Register = () => {
                 <h1 className='text-center text-5xl mb-8 text-white'>Register</h1>
                 <form className='space-y-5'>
                     <div className='max-w-sm mx-auto'>
-                        <input type="text" placeholder='First Name' className='w-full p-1 rounded-md'/>
+                        <input type="text" placeholder='First Name' name="firstName" value={values.firstName} className='w-full p-1 rounded-md' onChange={handleChange}/>
                     </div>
                     <div className='max-w-sm mx-auto'>
-                        <input type="text" placeholder='Last Name' className='w-full p-1 rounded-md'/>
+                        <input type="text" placeholder='Last Name' name="lastName" value={values.lastName} className='w-full p-1 rounded-md' onChange={handleChange}/>
                     </div>
                     <div className='max-w-sm mx-auto'>
-                        <input type="email" placeholder='Email' className='w-full p-1 rounded-md'/>
+                        <input type="email" placeholder='Email' name="email" value={values.email} className='w-full p-1 rounded-md' onChange={handleChange}/>
                     </div>
                     <div className='max-w-sm mx-auto'>
-                        <input type="password" placeholder='Password' className='w-full p-1 rounded-md'/>
+                        <input type="password" placeholder='Password' name="password" value={values.password} className='w-full p-1 rounded-md' onChange={handleChange}/>
                     </div>
                     <div className='max-w-sm mx-auto'>
-                        <textarea type="password" placeholder='About Me!!!' className='w-full p-1 rounded-md' rows="5"></textarea>
+                        <textarea type="password" placeholder='About Me!!!' name="about" value={values.about} className='w-full p-1 rounded-md' rows="5" onChange={handleChange}></textarea>
                     </div>
                     <div className='max-w-sm mx-auto'>
                         <button className='text-center w-full'>Submit</button>
